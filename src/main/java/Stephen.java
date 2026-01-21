645d381572126a151473b78515ee115bf482e8bb
HoangVu123456 <dkhv1508@gmail.com>
2026-01-22 07:46:48 +0800

    Level 0

---
+++ src/main/java/Stephen.java
@@ -0,0 +1,8 @@
+public class Stephen {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Stephen!");
+        System.out.println("What can I do for you today?");
+        System.out.println("_________________________________");
+        System.out.println("Bye, Hope to see you again soon!");
+    }
+}
