a0a9a297a1dd4ed5e50a65aa0a390f3f7db6aa3c
HoangVu123456 <dkhv1508@gmail.com>
2026-01-22 15:26:24 +0800

    Update Level 5

--- src/main/java/Stephen.java
+++ src/main/java/Stephen.java
@@ -46,62 +46,60 @@ public class Stephen {
                     }
                     history.get(index).unmark();
                     System.out.println("OK, I've marked this task as not done yet: " + history.get(index).toString());
-            } else if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
-                } else {
-                    if (input.startsWith("todo ")) {
-                        String description = input.substring(5).trim();
-                        if (description.isEmpty()) {
-                            throw new EmptyTaskException("The description for todo task can not be empty! Please try again!");
-                        }
-                        Task newTask = new ToDosTask(description);
-                        history.add(newTask);
-                        System.out.println("____________________________________________________________");
-                        System.out.println("Got it. I've added this task: ");
-                        System.out.println(newTask.toString());
-                        System.out.println("Now you have " + history.size() + " tasks in the list.");
-                        System.out.println("____________________________________________________________");
-                    } else if (input.startsWith("deadline ")) {
-                        String[] parts = input.substring(9).split(" /by ");
-                        if (parts.length != 2) {
-                            throw new WrongFormatException("Wrong format for deadline task command! Please use format: deadline <description> /by <time>");
-                        }
-                        String description = parts[0].trim();
-                        String by = parts[1].trim();
-                        if (description.isEmpty() || by.isEmpty()) {
-                            throw new EmptyTaskException("The command is missing essential information. The description and deadline cannot be empty.");
-                        }
-                        Task newTask = new DeadlinesTask(description, by);
-                        history.add(newTask);
-                        System.out.println("____________________________________________________________");
-                        System.out.println("Got it. I've added this task: ");
-                        System.out.println(newTask.toString());
-                        System.out.println("Now you have " + history.size() + " tasks in the list.");
-                        System.out.println("____________________________________________________________");
-                    } else if (input.startsWith("event ")) {
-                        String[] parts = input.substring(6).split(" /from | /to ");
-                        if (parts.length != 3) {
-                            throw new WrongFormatException("Wrong format for event task command! Please use format: event <description> /from <start> /to <end>");
-                        }
-                        String description = parts[0].trim();
-                        String from = parts[1].trim();
-                        String to = parts[2].trim();
-                        if (description.isEmpty() || from.isEmpty() || to.isEmpty()) {
-                            throw new EmptyTaskException("The command is missing essential information. The description, start time, and end time cannot be empty.");
-                        }
-                        Task newTask = new EventsTask(description, from, to);
-                        history.add(newTask);
-                        System.out.println("____________________________________________________________");
-                        System.out.println("Got it. I've added this task: ");
-                        System.out.println(newTask.toString());
-                        System.out.println("Now you have " + history.size() + " tasks in the list.");
-                        System.out.println("____________________________________________________________");
-                    } else {
-                        throw new InvalidInputException("Invalid input! I'm sorry, but I don't know what that means");
+                } else if (input.equals("bye")) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    break;
+                } else if (input.startsWith("todo ")) {
+                    String description = input.substring(5).trim();
+                    if (description.isEmpty()) {
+                        throw new EmptyTaskException("The description for todo task can not be empty! Please try again!");
                     }
+                    Task newTask = new ToDosTask(description);
+                    history.add(newTask);
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println(newTask.toString());
+                    System.out.println("Now you have " + history.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else if (input.startsWith("deadline ")) {
+                    String[] parts = input.substring(9).split(" /by ");
+                    if (parts.length != 2) {
+                        throw new WrongFormatException("Wrong format for deadline task command! Please use format: deadline <description> /by <time>");
+                    }
+                    String description = parts[0].trim();
+                    String by = parts[1].trim();
+                    if (description.isEmpty() || by.isEmpty()) {
+                        throw new EmptyTaskException("The command is missing essential information. The description and deadline cannot be empty.");
+                    }
+                    Task newTask = new DeadlinesTask(description, by);
+                    history.add(newTask);
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println(newTask.toString());
+                    System.out.println("Now you have " + history.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else if (input.startsWith("event ")) {
+                    String[] parts = input.substring(6).split(" /from | /to ");
+                    if (parts.length != 3) {
+                        throw new WrongFormatException("Wrong format for event task command! Please use format: event <description> /from <start> /to <end>");
+                    }
+                    String description = parts[0].trim();
+                    String from = parts[1].trim();
+                    String to = parts[2].trim();
+                    if (description.isEmpty() || from.isEmpty() || to.isEmpty()) {
+                        throw new EmptyTaskException("The command is missing essential information. The description, start time, and end time cannot be empty.");
+                    }
+                    Task newTask = new EventsTask(description, from, to);
+                    history.add(newTask);
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println(newTask.toString());
+                    System.out.println("Now you have " + history.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else {
+                    throw new InvalidInputException("Invalid input! I'm sorry, but I don't know what that means");
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
