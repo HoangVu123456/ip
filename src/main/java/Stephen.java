17907b46520a91fc79c58a6b42b084b4bab78d3b
HoangVu123456 <dkhv1508@gmail.com>
2026-01-22 08:46:19 +0800

    Level 3

--- src/main/java/Stephen.java
+++ src/main/java/Stephen.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Stephen {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> history = new ArrayList<>();
+        List<Task> history = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Stephen");
@@ -20,17 +20,35 @@ public class Stephen {
                     System.out.println("");
                 } else {
                     for (int i = 0; i < history.size(); i++) {
-                        System.out.println((i + 1) + ". " + history.get(i));
+                        System.out.println((i + 1) + ". " + history.get(i).toString());
                     }
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                System.out.println("____________________________________________________________");
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index >= 0 && index < history.size()) {
+                    history.get(index).mark();
+                    System.out.println("Nice! I've marked this task as done: " + history.get(index).toString());
+                } else {
+                    System.out.println("Invalid task number");
+                }
+            } else if (input.startsWith("unmark ")) {
+                System.out.println("____________________________________________________________");
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                if (index >= 0 && index < history.size()) {
+                    history.get(index).unmark();
+                    System.out.println("OK, I've marked this task as not done yet: " + history.get(index).toString());
+                } else {
+                    System.out.println("Invalid task number");
+                }
             } else if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
             } else {
-                history.add(input);
+                history.add(new Task(input));
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + input);
                 System.out.println("____________________________________________________________");
