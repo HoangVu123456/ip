4c875f1de38ea2024965c4447395d4881a1de0a3
HoangVu123456 <dkhv1508@gmail.com>
2026-01-22 08:02:41 +0800

    Level 1

--- src/main/java/Stephen.java
+++ src/main/java/Stephen.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Stephen {
     public static void main(String[] args) {
-        System.out.println("Hello, I'm Stephen!");
-        System.out.println("What can I do for you today?");
-        System.out.println("_________________________________");
-        System.out.println("Bye, Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Stephen");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = sc.nextLine().trim();
+
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("list");
+                System.out.println("____________________________________________________________");
+            } else if (input.equals("blah")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("blah");
+                System.out.println("____________________________________________________________");
+            } else if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
+        sc.close();
     }
 }
